35f03d3d348e4cc004e67590015eadd208bfc9c7
Mikhail Ustyantsev <mustyantsev@lohika.com>
2023-11-10 13:15:03 +0200

    cleanup

--- .github/workflows/create_auto_pr.sh
+++ .github/workflows/create_auto_pr.sh
@@ -8,7 +8,7 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 BRANCH_NAME="automatic-update-to-client-cpp-$NEW_VERSION"
 
 git checkout -b $BRANCH_NAME
-
+echo "after new branch"
 # Update conanfile.py
 conanfile_path=$CONAN_FILE
 config_conan=$(cat $conanfile_path)
@@ -16,19 +16,19 @@ search_line='self.requires("opentdf-client/'
 new_conanfile_content=$(echo "$config_conan" | sed "s|${search_line}[0-9.]*@|${search_line}${NEW_VERSION}@|")
 echo "$new_conanfile_content" > "$conanfile_path"
 git add "$conanfile_path"
-
+echo "after add1"
 # Update build.yml
 build_yml_path=$WORKFLOW_FILE
 config_yaml=$(cat $build_yml_path)
 new_build_yml_content=$(echo "$config_yaml" | sed "s/VCLIENT_CPP_VER: .*/VCLIENT_CPP_VER: $NEW_VERSION/")
 echo "$new_build_yml_content" > "$build_yml_path"
 git add "$build_yml_path"
-
+echo "after add2"
 # Commit changes
 git commit -m "Automatic update to client-cpp $NEW_VERSION"
-
+echo "after commit"
 git push --set-upstream origin HEAD:"$BRANCH_NAME" -f
-git status
+
 sleep 5
 gh pr create \
     --body "Automated PR created by GitHub Actions" \
